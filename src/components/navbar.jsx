ec810bd5c89572c892b30a7ea17d899f2bf3b620
Bimo Dwien Prabowo <bimodwien2@gmail.com>
2024-04-19 16:09:17 +0700

    penambahan react-router-dom

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,15 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <div>Navbar</div>
+      <div className="flex items-center justify-between px-10 py-8">
+        <div>Logo</div>
+        <div className="flex gap-5 pr-6">
+          <div>Home</div>
+          <div>About</div>
+          <div>Profile</div>
+          <div>Hilih</div>
+        </div>
+      </div>
     </>
   );
 };
