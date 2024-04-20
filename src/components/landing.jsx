9ec89b1ca8c36db4a42d19ef7eda538e52ad54e3
Bimo Dwien Prabowo <bimodwien2@gmail.com>
2024-04-20 18:12:45 +0700

    navbar done

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Landing = () => {
   return (
     <>
-      <div>Landing</div>
+      <div className="p-4">Landing</div>
     </>
   );
 };
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <>
-      <div className="flex items-center justify-between px-10 py-8">
-        <div>Logo</div>
-        <div className="flex gap-5 pr-6">
-          <div>Home</div>
-          <div>About</div>
-          <div>Profile</div>
-          <div>Hilih</div>
+      <nav className="bg-gray-800 p-4">
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="text-white text-lg font-semibold">Company</h1>
+          </div>
+          <div className="lg:hidden">
+            <button
+              onClick={toggleMenu}
+              className="text-white focus:outline-none focus:text-white"
+            >
+              <FontAwesomeIcon icon={faBars} />
+            </button>
+          </div>
+          <ul className={`lg:flex ${isOpen ? "block" : "hidden"} mt-4 lg:mt-0`}>
+            <li className="mr-6">
+              <a className="text-white" href="#home">
+                Home
+              </a>
+            </li>
+            <li className="mr-6">
+              <a className="text-white" href="#about">
+                About
+              </a>
+            </li>
+            <li className="mr-6">
+              <a className="text-white" href="#services">
+                Services
+              </a>
+            </li>
+            <li className="mr-6">
+              <a className="text-white" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
-      </div>
+      </nav>
     </>
   );
 };
