9655ffd26742e0002ddd7cdf6295e3bbe25539cd
Bimo Dwien Prabowo <bimodwien2@gmail.com>
2024-04-23 16:06:56 +0700

    penambahan landing page

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,9 +1,86 @@
 import React from "react";
+import ars1 from "../assets/arsitek1.png";
+import rec1 from "../assets/Rectangle9.png";
+import rec2 from "../assets/Rectangle4.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 const Landing = () => {
   return (
     <>
-      <div className="p-4">Landing</div>
+      <section>
+        <div className="">
+          <div className="relative flex justify-center items-center h-full w-full">
+            <img src={ars1} alt="arsitek gedung" className="object-cover" />
+            <div className="absolute font-semibold text-xl italic md:text-5xl lg:font-bold lg:text-8xl text-[#DEDEDE]">
+              Harmony Interior
+            </div>
+          </div>
+
+          <div className="text-base leading-4 px-4 pt-20 tracking-[1.5px] text-gray-500 uppercase lg:px-10">
+            Harmony Interior
+          </div>
+          <div className="text-3xl px-4 pt-6 leading-10 tacking-[-0.5px] font-bold w-auto text-[#DEDEDE] lg:pb-10 lg:px-10">
+            Where Interior Dreams Come to Life
+          </div>
+          <div className="px-4 pt-20 uppercase text-gray-500 text-base leading-5 tracking-[1.5px] lg:px-10">
+            crafting homes, creating memories
+          </div>
+          <div className="px-4 pt-6 text-3xl font-bold leading-10 w-auto tracking-[-0.5px] text-[#DEDEDE] lg:px-10">
+            At Harmony Interior, we don't just design interiors; we craft
+            experiences that resonate with your soul.
+          </div>
+        </div>
+      </section>
+
+      <section className="lg:px-10">
+        <div className="pt-16 px-4">
+          <div className="flex flex-row gap-5 items-center uppercase text-lg font-semibold w-auto text-[#DEDEDE] leading-5 tracking-[0.5px]">
+            <FontAwesomeIcon icon={faArrowDown} /> explore our world
+          </div>
+        </div>
+        <div className="lg:flex lg:flex-row-reverse lg:pb-16 lg:pt-32 lg:gap-5 lg:items-center px-4 pt-12 lg:justify-between lg:w-full">
+          <div className="lg:w-2/4">
+            <div className="uppercase text-gray-500 text-base w-auto leading-4 tracking-[1.5px] pb-5">
+              unvieling our legacy
+            </div>
+            <div className="text-[30px] leading-10 tracking-[-0.5px] font-medium pt-4 pb-14">
+              Step into our world and discover the story behind Harmony
+              Interior. From humble beginnings to a powerhouse of creativity,
+              learn about our journey, our values, and the team that drives our
+              vision forward.
+            </div>
+          </div>
+          <div className="flex justify-center items-center ">
+            <img
+              src={rec1}
+              alt="rectangular_1"
+              className="w-[366px] h-[577px]"
+            />
+          </div>
+        </div>
+
+        <div className="px-4 pt-12 lg:flex lg:flex-row lg:py-24 lg:gap-5 lg:items-center lg:justify-between lg:2:full pb-3">
+          <div className="lg:w-2/4">
+            <div className="uppercase text-gray-500 text-base w-auto leading-4 tracking-[1.5px] pb-5">
+              Discover Our Masterpieces
+            </div>
+            <div className="text-[30px] leading-10 tracking-[-0.5px] font-medium pt-4 pb-14">
+              Dive into our portfolio of exquisite designs and witness the magic
+              of our craftsmanship. Whether it's revamping a cozy living room or
+              transforming a corporate space, our creations speak volumes of
+              style, elegance, and functionality.
+            </div>
+          </div>
+          <div className="flex justify-center items-center">
+            <img
+              src={rec2}
+              alt="rectangular_2"
+              className="w-[366px] h-[577px]"
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,10 +11,10 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="bg-gray-800 p-4">
+      <nav className="bg-gray-800 p-4 relative lg:px-10">
         <div className="flex justify-between items-center">
           <div>
-            <h1 className="text-white text-lg font-semibold">Company</h1>
+            <h1 className="text-white text-lg font-semibold">Harmony</h1>
           </div>
           <div className="lg:hidden">
             <button
@@ -24,25 +24,37 @@ const Navbar = () => {
               <FontAwesomeIcon icon={faBars} />
             </button>
           </div>
-          <ul className={`lg:flex ${isOpen ? "block" : "hidden"} mt-4 lg:mt-0`}>
-            <li className="mr-6">
+          <ul
+            className={`lg:flex ${
+              isOpen ? "block" : "hidden"
+            } mt-0 lg:mt-2 lg:w-1/2 lg:h-10 pl-2 lg:justify-end lg:items-center h-screen absolute top-0 bg-gray-800 right-0`}
+          >
+            <li className="flex flex-row-reverse p-0 m-0">
+              <button
+                className="text-white lg:hidden bg-red-700 py-1 px-2 rounded-sm mb-5"
+                onClick={toggleMenu}
+              >
+                <FontAwesomeIcon icon={faX} className="self-end justify-end" />
+              </button>
+            </li>
+            <li className="mr-8 border-b-2 pt-3 lg:pt-0">
               <a className="text-white" href="#home">
                 Home
               </a>
             </li>
-            <li className="mr-6">
+            <li className="mr-8 border-b-2 pt-3 lg:pt-0">
               <a className="text-white" href="#about">
-                About
+                About Us
               </a>
             </li>
-            <li className="mr-6">
+            <li className="mr-8 border-b-2 pt-3 lg:pt-0">
               <a className="text-white" href="#services">
                 Services
               </a>
             </li>
-            <li className="mr-6">
-              <a className="text-white" href="#contact">
-                Contact
+            <li className="mr-8 border-b-2 pt-3 lg:pt-0">
+              <a className="text-white" href="#team">
+                Teams
               </a>
             </li>
           </ul>
