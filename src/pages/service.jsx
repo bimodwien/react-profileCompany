4274b4c2528d836f034537fe8326092df4edcea7
bimodwien <bimodwien2@gmail.com>
2024-04-25 20:30:45 +0700

    service page done

--- src/pages/service.jsx
+++ src/pages/service.jsx
@@ -28,6 +28,23 @@ const Service = () => {
         </div>
         <CardService />
       </section>
+
+      <section className="px-4 lg:px-10">
+        <div>
+          <div className="text-base text-gray-500 uppercase leading-4 tracking-[1.5px] pt-10 pb-5">
+            Ready to Elevate Your Space
+          </div>
+          <div className="text-3xl leading-10 tracking-[-0.5px] font-bold pt-4 pb-10 text-[#DEDEDE]">
+            Explore our full reange of products and services and take the step
+            towards creating the home of your dreams. Contact us today to learn
+            more and schedule a consultation with our team of experts.
+          </div>
+        </div>
+
+        <div className="flex justify-center align-center pt-3 pb-10">
+          <button className="bg-[#9D9D9D] px-10 py-3 rounded-sm font-medium text-xl hover:bg-gray-700 hover:border-white ">Contact Us</button>
+        </div>
+      </section>
     </>
   );
 };
